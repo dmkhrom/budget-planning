5d4a74a6a39ac060000a37c9c34b2b5cf763ce5d
dmkhrom <d.khramykh@softteco.com>
2022-07-01 20:20:26 +0300

    - add ui changes for big numbers

--- src/features/SecondTab/ChannelRow/components/BreakDownItemValue.jsx
+++ src/features/SecondTab/ChannelRow/components/BreakDownItemValue.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Tooltip } from 'components/Tooltip/Tooltip';
 import { ItemValue } from 'features/SecondTab/ChannelRow/styles';
 
-const MAX_VISIBLE_SYMBOLS = 8;
+const MAX_VISIBLE_SYMBOLS = 9;
 
 export const BreakDownItemValue = ({ value }) => {
 
