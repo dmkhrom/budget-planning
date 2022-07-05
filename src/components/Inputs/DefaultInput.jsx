d34c58d0d18593f1436e80b017fd06d6a8e47628
dmkhrom <d.khramykh@softteco.com>
2022-07-05 15:18:14 +0300

    - fix eslint

--- src/components/Inputs/DefaultInput.jsx
+++ src/components/Inputs/DefaultInput.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { CustomInput } from 'components/Inputs/styles';
 
-export const DefaultInput = ({ inputRef, ...props }) => console.log(inputRef) || <CustomInput ref={inputRef} {...props} />;
+export const DefaultInput = ({ inputRef, ...props }) => <CustomInput ref={inputRef} {...props} />;
 
 DefaultInput.propTypes = {
 	inputRef: PropTypes.oneOfType([
