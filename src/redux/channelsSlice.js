11a3de13eec4050156393b886c5e598beb089480
dmkhrom <d.khramykh@softteco.com>
2022-07-05 15:36:05 +0300

    - refactoring slice

--- src/redux/channelsSlice.js
+++ src/redux/channelsSlice.js
@@ -16,10 +16,8 @@ const channelsSliceReducer = createSlice({
 			state.channels = state.channels.filter((item) => item.id !== payload);
 		},
 		updateChannelsData: (state, { payload }) => {
-			const immediateChannels = [...state.channels];
-			const channelIndex = immediateChannels.findIndex((item) => item.id === payload.id);
-			immediateChannels[channelIndex] = payload;
-			state.channels = immediateChannels;
+			const channelIndex = state.channels.findIndex((item) => item.id === payload.id);
+			state.channels[channelIndex] = payload;
 		}
 	}
 });
